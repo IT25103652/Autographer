1702985a0767be773c421f00a5aaaa0dc5bb4d0f
IT25103652 <cspadmasekara@gmail.com>
2026-06-27 15:54:45 +0530

    Feat: Re-enable maintenance mode

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const IS_MAINTENANCE_MODE = false;
+const IS_MAINTENANCE_MODE = true;
 
 export function middleware(request: NextRequest) {
   if (IS_MAINTENANCE_MODE) {
